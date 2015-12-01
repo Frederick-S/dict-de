00647bac66afd10eafbd4a0e4821b97596cdc3ff
Frederick-S <mao_xiaodan@hotmail.com>
2015-12-01 18:39:59 +0800

    Ignore empty word examples

--- test.js
+++ test.js
@@ -6,19 +6,19 @@ describe('Query word', function () {
     it('Query empty string', function (done) {
         query('').done(function (data) {}, function (message) {
             assert.equal('Invalid word.', message);
-            
+
             done();
         });
     });
-    
+
     it('Query non empty string', function (done) {
         query('fahren').done(function (data) {
             assert.notEqual('Request error.', data);
-            
+
             done();
         }, function (message) {
             assert.equal('Request error.', message);
-            
+
             done();
         });
     });
@@ -28,35 +28,55 @@ describe('Parse data', function () {
     it('Parse invalid word', function (done) {
         query('sdf8234').done(function (data) {
             assert.notEqual('Request error.', data);
-            
+
             var word = parse(data);
-            
+
             assert.equal(false, word.valid);
-            
+
             done();
         }, function (message) {
             assert.equal('Request error.', message);
-            
+
             done();
         });
     });
-    
+
     it('Parse valid word', function (done) {
         query('fahren').done(function (data) {
             assert.notEqual('Request error.', data);
-            
+
             var word = parse(data);
-            
+
             assert.equal('fahren', word.name);
             assert.equal(true, word.valid);
             assert.equal(10, word.explanations.length);
             assert.equal(8, word.examples.length);
-            
+
             done();
         }, function (message) {
             assert.equal('Request error.', message);
-            
+
             done();
         });
     });
+
+    it('Ignore invalid word examples', function (done) {
+        query('Name').done(function (data) {
+            assert.notEqual('Request error.', data);
+
+            var word = parse(data);
+
+            assert.equal('Name', word.name);
+            assert.equal(true, word.valid);
+            assert.equal(9, word.explanations.length);
+            assert.equal(3, word.examples.length);
+
+            done();
+        }, function (message) {
+            assert.equal('Request error.', message);
+
+            done();
+        });
+    });
+
 });
